0d36de15f65e8215ef565b7588f676f83ffb18aa
JIMENA <jimena.hernandez0051@gmail.com>
2025-10-21 17:18:59 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-const jell = document.querySelector('.honguito');
+const honguito = document.querySelectorAll('.honguito');
+const contadorDeMonedas = document.querySelector('.contador-de-monedas');
 const personaje = document.querySelector('.personaje');
-let counter = 0;
+const rectangulo = document.querySelector('.rectangulo');
+let contador = 0;
 
-console.log(jell, personaje);
+let monedas = 0;
+let posX = 7;  
+let posY = 320;
+
+honguito.forEach( honguito => {
+   
+    honguito.addEventListener('click', () => {
+         honguito.style.filter = "grayscale(1)";
+         honguito.classList.add('saltar');
+});
+
+})
+
+/* personaje.forEach( personaje => {
+    personaje.style.filter = "grayscale(1)";
+ */
