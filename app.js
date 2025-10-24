e50b4c5e32d7246f7a283dba007c99f7398acc33
JIMENA <jimena.hernandez0051@gmail.com>
2025-10-24 17:13:03 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -17,6 +17,42 @@ honguito.forEach( honguito => {
 
 })
 
-/* personaje.forEach( personaje => {
-    personaje.style.filter = "grayscale(1)";
- */
+ /////////////////////////////////////FIN DEL PRIMER JUEFGO//////////////////////////////////////////
+ const honguito = document.querySelectorAll('.honguito');
+const contadorDeMonedas = document.querySelector('.contador-de-monedas');
+const personaje = document.querySelector('.personaje');
+const rectangulo = document.querySelector('.rectangulo');
+let contador = 0;
+
+let monedas = 0;
+let posX = 7;  
+let posY = 320;
+
+honguito.forEach( honguito => {
+   
+    honguito.addEventListener('click', () => {
+         honguito.style.filter = "grayscale(1)";
+         honguito.classList.add('saltar');
+});
+
+})
+ /////////////////////////////////////FIN DEL SEGUNDO JUEFGO//////////////////////////////////////////
+ const honguito = document.querySelectorAll('.honguito');
+const contadorDeMonedas = document.querySelector('.contador-de-monedas');
+const personaje = document.querySelector('.personaje');
+const rectangulo = document.querySelector('.rectangulo');
+let contador = 0;
+
+let monedas = 0;
+let posX = 7;  
+let posY = 320;
+
+honguito.forEach( honguito => {
+   
+    honguito.addEventListener('click', () => {
+         honguito.style.filter = "grayscale(1)";
+         honguito.classList.add('saltar');
+});
+
+})
+ /////////////////////////////////////FIN DEL SEGUNDO JUEFGO//////////////////////////////////////////
