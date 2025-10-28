c811ad97ab0bc16e7f1bcb0ed030fe2988cc0d3f
JIMENA <jimena.hernandez0051@gmail.com>
2025-10-28 15:56:38 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+
+
+ /////////////////////////////////////FIN DEL PRIMER JUEFGO//////////////////////////////////////////
+
 const honguito = document.querySelectorAll('.honguito');
 const contadorDeMonedas = document.querySelector('.contador-de-monedas');
 const personaje = document.querySelector('.personaje');
@@ -16,11 +20,46 @@ honguito.forEach( honguito => {
 });
 
 })
-
- /////////////////////////////////////FIN DEL PRIMER JUEFGO//////////////////////////////////////////
-
-
  /////////////////////////////////////FIN DEL SEGUNDO JUEFGO//////////////////////////////////////////
 
 
  /////////////////////////////////////FIN DEL SEGUNDO JUEFGO//////////////////////////////////////////
+const escena = document.querySelectorAll (".escena")
+const anterior = document.querySelector (".anterior")
+const siguiente = document.querySelector (".siguiente")
+const fondo = document.querySelector (".fondo")
+
+let indice = 0
+
+console.log(escena)
+console.log(anterior)
+console.log(siguiente)
+console.log(fondo)
+
+function mostrarEscena(i){
+    for (let j = 0; j < escena.length; j++) {
+        escenas[j].classList.add("escena")
+
+    }
+
+    escena[i].classList.add("fondo")
+    indice = i
+}
+anterior.addEventListener("click", function(){
+    indice = indice - 1
+    if(indice < 0){
+        indice = indice.length - 1 //va a la ultima imagen
+
+    }
+    mostrarEscena(indice)
+})
+siguiente.addEventListener("click", function(){
+    indice = indice + 1
+    if(indice < 0){
+        indice = indice.length - 1 //va a la ultima imagen
+
+    }
+    mostrarEscena(indice)
+})
+
+
