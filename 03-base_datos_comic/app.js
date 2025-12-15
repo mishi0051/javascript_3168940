131bf8324d776e0db27544acdd2360f6da28cc5e
JIMENA <jimena.hernandez0051@gmail.com>
2025-12-15 16:23:43 -0500

    Delete 03-base_datos_comic directory

--- 03-base_datos_comic/app.js
+++
@@ -1,9 +0,0 @@
-const cabezaDePagina = document.querySelector(".hero-content");
-
-console.log(cabezaDePagina);
-
-cabezaDePagina.innerHTML = 
-`
-    <h1>${comic.nombreComic}</h1>
-    <p>${comic.genero}</p>
-`
